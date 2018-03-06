e5f270eff56a852512e74c5118bc1766874f96bc
Dawanit <newsatit97@gmail.com>
2018-03-06 05:17:24 +0000

    Added show/hide links regarding auth

--- app.js
+++ app.js
@@ -32,6 +32,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// (middleware) pass user object to every routes
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
 //============
 // ROUTES
 //============
