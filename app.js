f8ba4318bfceab3abb77b664093cf5cd45449f1a
Dawanit <newsatit97@gmail.com>
2018-04-17 07:52:57 +0000

    Add routes/blogs.js

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var express         = require("express"),
     User            = require("./models/user"),
     Comment         = require("./models/comment");
 
+var blogRoutes = require("./routes/blogs");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/movie_app");
 app.use(express.static("public"));
@@ -127,49 +130,8 @@ app.get("/moviews/:id", function(req, res) {
    res.render("show"); 
 });
 
-app.get("/blogs", function(req, res) {
-    // Get all blogs from DB
-    Blog.find({}, function(err, allBlogs){
-       if(err){
-           console.log(err);
-       } else {
-          res.render("blog posts/blogs",{blogs:allBlogs});
-       }
-    });
-});
+app.use(blogRoutes);
 
-app.post("/blogs", function(req, res){
-    // get data from form and add to blogs array
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.body.description;
-    var newPost = {name: name, image: image, description: desc}
-    // Create a new campground and save to DB
-    Blog.create(newPost, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect back to campgrounds page
-            res.redirect("/blogs");
-        }
-    });
-});
-app.get("/blogs/new", function(req, res){
-   res.render("blog posts/newPost"); 
-});
-
-app.get("/blogs/:id", function(req, res){
-    //find the campground with provided ID
-    Blog.findById(req.params.id).populate("comments").exec(function(err, foundPost){
-        if(err){
-            console.log(err);
-        } else {
-            console.log(foundPost)
-            //render showPosts template with that post
-            res.render("blog posts/showPosts", {blog: foundPost});
-        }
-    });
-})
 
 // ====================
 // COMMENTS ROUTES
---
+++ routes/blogs.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router();
+var Blog = require("../models/blog")
+router.get("/blogs", function(req, res) {
+    // Get all blogs from DB
+    Blog.find({}, function(err, allBlogs){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("blog posts/blogs",{blogs:allBlogs});
+       }
+    });
+});
+
+router.post("/blogs", function(req, res){
+    // get data from form and add to blogs array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newPost = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Blog.create(newPost, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/blogs");
+        }
+    });
+});
+router.get("/blogs/new", function(req, res){
+   res.render("blog posts/newPost"); 
+});
+
+router.get("/blogs/:id", function(req, res){
+    //find the campground with provided ID
+    Blog.findById(req.params.id).populate("comments").exec(function(err, foundPost){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundPost)
+            //render showPosts template with that post
+            res.render("blog posts/showPosts", {blog: foundPost});
+        }
+    });
+})
+
+module.exports = router;
